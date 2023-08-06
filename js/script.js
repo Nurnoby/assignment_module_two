0ccb79e040430e622e5e4d3bc03092a95521ea2d
Md. Nur Nobi <nurnobyislam206@gmail.com>
2023-08-06 15:35:01 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// dom manupulation 
+function domManupulate(){
+    let x=document.getElementById('changeDiv');
+    x.innerHTML="Dom is Manupulated";
+}
+
+
+// click counter
+function count(){
+
+    var element=document.getElementById('showCount');
+    var value=element.innerHTML;
+    ++value;
+    console.log(value);
+    document.getElementById('showCount').innerHTML=value;
+    
+}
+// image switcher
+function imageOne(){
+    var myImageOne=document.getElementById('changeImage');
+    myImageOne.src="../images/image_one.jpg";
+}
+
+function imageTwo(){
+    var myImageOne=document.getElementById('changeImage');
+    myImageOne.src="../images/image_two.jpg";
+}
+
+// Ajax
+function getData(){
+        var URL="https://jsonplaceholder.typicode.com/posts";
+        var requestOptions= {method:'GET'};
+        fetch(URL,requestOptions)
+        .then(response=> response.json())
+        .then(result=> console.log(result))
+        .catch(err=>console.log('error', err));
+}
